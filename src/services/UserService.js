f4d1bb534621b08b199f273df196a16df99065ac
vishw patel <66671671+vizzv@users.noreply.github.com>
2025-07-25 10:49:17 +0530

    Update UserService.js

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,27 +1,36 @@
 import axios from 'axios';
 
 const USER_API_BASE_URL = "https://email-campaign-be.azurewebsites.net/api/v1/users";
+const SECRET_TOKEN = import.meta.env.VITE_SECRET_TOKEN; // For Vite
+// const SECRET_TOKEN = process.env.REACT_APP_SECRET_TOKEN; // For Create React App
+
+const config = {
+  withCredentials: true,
+  headers: {
+    'X-Secret-Token': SECRET_TOKEN
+  }
+};
 
 class UserService {
 
     getUsers(){
-        return axios.get(USER_API_BASE_URL,{ withCredentials: true });
+        return axios.get(USER_API_BASE_URL,confih);
     }
 
     createUser(user){
-        return axios.post(USER_API_BASE_URL, user,{ withCredentials: true });
+        return axios.post(USER_API_BASE_URL, user,config);
     }
 
     getUserById(userId){
-        return axios.get(USER_API_BASE_URL + '/' + userId,{ withCredentials: true });
+        return axios.get(USER_API_BASE_URL + '/' + userId,config);
     }
 
     updateUser(user, userId){
-        return axios.put(USER_API_BASE_URL + '/' + userId, user,{ withCredentials: true });
+        return axios.put(USER_API_BASE_URL + '/' + userId, user,config);
     }
 
     deleteUser(userId){
-        return axios.delete(USER_API_BASE_URL + '/' + userId,{ withCredentials: true });
+        return axios.delete(USER_API_BASE_URL + '/' + userId,config);
     }
 }
 
